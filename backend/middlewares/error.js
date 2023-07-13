67dfcdba536a6d77a29af933164bc8c7a825069d
acer <vova.mkhitaryan94@gmail.com>
2023-07-13 22:42:03 +0400

    adjusting errorhandling

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,16 +1,18 @@
 const IncorrectError = require('../utils/IncorrectError');
-const NotFoundError = require('../utils/NotFoundError');
+// const NotFoundError = require('../utils/NotFoundError');
 const ConflictError = require('../utils/ConflictError');
-const WrongDataError = require('../utils/AuthorizationError');
-const LimitedAccessError = require('../utils/LimitedAccessError');
+// const WrongDataError = require('../utils/AuthorizationError');
+// const LimitedAccessError = require('../utils/LimitedAccessError');
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let error = err;
-console.log(err);
+  // console.log(err);
 
   if (err.code === 11000) {
     error = new ConflictError('Пользователь с таким имейлом уже существует');
+  } else if (err.name === 'CastError') {
+    error = new IncorrectError('Переданы некоректные данные');
   }
 
   res
