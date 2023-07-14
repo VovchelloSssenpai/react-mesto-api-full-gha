106d12161996b4d594582e7147961b04d599d890
acer <vova.mkhitaryan94@gmail.com>
2023-07-14 10:35:17 +0400

    adjusting errorhandling

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,26 +1,22 @@
 const IncorrectError = require('../utils/IncorrectError');
 const ConflictError = require('../utils/ConflictError');
+const { DEFAULT_ERROR_CODE } = require('../utils/utils')
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let error = err;
-  // console.log(err);
+   console.log(err);
 
   if (err.code === 11000) {
     error = new ConflictError('Пользователь с таким имейлом уже существует');
   } else if (err.name === 'CastError') {
     error = new IncorrectError('Переданы некоректные данные');
-  } else {
-    error = {
-      statusCode: 500,
-      message: 'Внутренняя ошибка',
-    };
   }
 
   res
-    .status(error.statusCode)
+    .status(error.statusCode || DEFAULT_ERROR_CODE)
     .send({
-      message: error.message,
+      message: error.statusCode ? error.message : 'Внутренняя ошибка',
     });
 };
 
